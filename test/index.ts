b7003b70f5472f1ff49f39b289fbcac23a569642
NikinikitaN <119474174+NikinikitaN@users.noreply.github.com>
2023-09-22 19:08:16 +0530

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -3,5 +3,3 @@ import { ethers } from "hardhat";
 
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 //describe("UsageBasedInsurance", function () {});
-// for chai - npm install --save-dev @nomicfoundation/hardhat-chai-matchers 
-// for hardhat - npm install --save-dev @nomicfoundation/hardhat-chai-matchers chai @nomicfoundation/hardhat-ethers ethers.
